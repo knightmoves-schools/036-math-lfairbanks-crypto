f6196cc31295433c46791816d4959c8e3729e9bb
luke <lfairbanks@anamosa.k12.ia.us>
2025-10-30 18:39:09 +0000

    done

---
+++ index.js
@@ -0,0 +1,6 @@
+let sum = 1 + 4;
+let difference = sum - 2;
+let product = difference * 5;
+let quotient = product / 2;
+let remainder = product % 2;
+document.getElementById('result').innerHTML = quotient;
